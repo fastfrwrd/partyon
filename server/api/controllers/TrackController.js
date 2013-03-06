b3afb13e248939cc16dd918b524b2d737d3ef390
Paul Marbach <fastfrwrd@gmail.com>
2013-03-06 17:06:58 -0600

    autocomplete and adding tracks to a party

---
+++ server/api/controllers/TrackController.js
@@ -0,0 +1,25 @@
+module.require('underscore');
+/*---------------------
+	:: Track 
+	-> controller
+---------------------*/
+var TrackController = {
+
+	// To trigger this action locally, visit: `http://localhost:port/track/create`
+	create: function (req,res,next) {
+		Track.find({
+			trackUri : req.param('trackUri'),
+			partyId : req.param('partyId')
+		}).done(function(err, track) {
+			if(err) res.json({err:err}, 500);
+			if(track) {
+				Track.update({id : track.id}, { votes : ++track.votes }, function(err, track) {
+					if(err) return res.json(err, 500);
+					else return res.json(track, 200);
+				});
+			} else return next();
+		});
+	}
+
+};
+module.exports = TrackController;
--- server/api/models/Track.js
+++ server/api/models/Track.js
@@ -6,12 +6,8 @@ module.exports = {
 
 	attributes	: {
 		trackUri: 'STRING',
-		artistUri: 'STRING',
-		albumUri: 'STRING',
-
 		title: 'STRING',
 		artist: 'STRING',
-		album: 'STRING',
 		partyId: "INTEGER"
 	}
 
