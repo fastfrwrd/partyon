8131f190136222988b1a7d972ca94e5b97c314d0
Paul Marbach <fastfrwrd@gmail.com>
2013-03-06 00:30:59 -0600

    progress on the ejs for the view

--- server/api/controllers/PartyController.js
+++ server/api/controllers/PartyController.js
@@ -6,20 +6,25 @@ var PartyController = {
 
 	// To trigger this action locally, visit: `http://localhost:port/party/create`
 	create: function (req,res,next) {
-		var generateUri = function(name, id) {
-			// URI to slug if name, else we make it "p<ID>"
-			var uri = (name && name.length) ? name.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'') : "p" + id;
+		Party.findAll().done(function(err, parties) {
+			if(err || !parties) parties = [];
+			var name = req.param('name'),
+				id = parties.length + 1;
+			// URI to slug if name
+			if(name && name.length) {
+				uri = name.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
+			} else {
+				//else we make it "p<ID>" and 
+				uri = "p" + id;
+				name = "Fabulous Party";
+			}
 			// Check if there's a party with the same slug, and tack the ID on the end if so
 			if(Party.findByName(uri).length) uri += "-" + id;
 
-			return uri;
-		};
-
-		Party.findAll().done(function(err, parties) {
-			if(err || !parties) parties = [];
-			req.params.uri = generateUri(req.param('name'), parties.length + 1);
+			req.params.uri = uri;
+			req.params.name = name;
+			next();
 		});
-		next();
 	},
 
 	view: function(req,res,next) {
@@ -31,9 +36,8 @@ var PartyController = {
 			User.findAllByPartyId(party.id).done(function(err,users) { party.users = users; });
 			Track.findAllByPartyId(party.id).done(function(err,tracks) { party.tracks = tracks; });
 
-			res.json(party);
+			res.view('party/view', { party : party });
 		});
-
 	}
 
 };
